fe45eed9219fcaa7cf85db6e4a5bbbb330398a9b
Kaio Henrique <kaiohenriqueranking1@gmail.com>
2022-10-13 12:21:42 -0300

    adicionando middleware de cors

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 const db = require('./database/db')
 const routes = require('./routes/routes')
 
@@ -8,6 +9,26 @@ const app = express()
 //conexão com o banco de dados
 db.connect()
 
+//Endereços permitidos para acessar a api
+
+const allowedOrigins = [
+  'http://127.0.0.1:5500',
+  'http://www.siteteste.com.br'
+]
+//Habilitando o cors
+app.use(cors({
+  origin: function(origin, callback) {
+    let allowed = true
+
+    // app mobile
+    if(!origin) allowed = true
+
+    if(!allowedOrigins.includes(origin)) allowed = false
+
+    callback(null, allowed)
+  }
+}))
+
 //Habilita o server para receber dados json
 app.use(express.json())
 
@@ -15,5 +36,5 @@ app.use(express.json())
 app.use('/api', routes)
 
 //Iniciando o servidor
-const port = process.env.PORT || 5500
+const port = process.env.PORT || 5000
 app.listen(port, () => console.log(`Servidor rodando na porta ${port}`))
